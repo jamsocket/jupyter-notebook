b7898b2f27a26ec04bcbd174cf6886d19d182692
Taylor Baldwin <taylorbaldwin@gmail.com>
2022-03-09 16:33:04 -0500

    add some helpful config options to the jupyter notebook

--- run.sh
+++ run.sh
@@ -3,4 +3,10 @@
 jupyter notebook --generate-config
 echo "c.NotebookApp.token = '$JAMSOCKET_JUPYTER_TOKEN'" >> ~/.jupyter/jupyter_notebook_config.py
 
+# automatically redirect / to a notebook by uncommenting and editing the following line:
+# echo "c.NotebookApp.default_url = '/notebooks/[YOUR NOTEBOOK HERE].ipynb'" >> ~/.jupyter/jupyter_notebook_config.py
+
+# to embed the jupyter notebook in an iframe, uncomment the following line and edit your domain
+# echo "c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors \'self\' [YOUR DOMAIN];'}}" >> ~/.jupyter/jupyter_notebook_config.py
+
 jupyter notebook --ip 0.0.0.0 --no-browser
